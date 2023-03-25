0b4ede5da963859d785af233cff8bfcb6aad4a88
luisfejaramillor <luisfejaramillor@gmail.com>
2023-03-25 09:40:23 -0500

    Updated data file

--- 02-http/data.js
+++ 02-http/data.js
@@ -1,15 +1,18 @@
  const obj = [
   {
+    id: 1,
     name: "Reloj",
     price: 100,
     quantity: 2,
   },
   {
+    id: 2,
     name: "Correa",
     price: 100,
     quantity: 3,
   },
   {
+    id: 3,
     name: "Sombrero",
     price: 1000,
     quantity: 3,
