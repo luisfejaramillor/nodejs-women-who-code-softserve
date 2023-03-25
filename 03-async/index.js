20e7ae01d9b51fa1dfb0d051575fd2e0c29524cf
luisfejaramillor <luisfejaramillor@gmail.com>
2023-03-25 15:26:07 -0500

    Added index,js

---
+++ 03-async/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const PORT = 3000
+
+const app = express()
+
+const {obj} = require("../02-http/data")
+const endPoint = '/api/v2/users'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const errorLogger = (err, req, res, next)=> {
+console.log('halkllo')
+next(err)
+}
+
+const errorHandler = (err, req, res, next)=> {
+console.log(err)
+// res.status(400)
+res.json(err)
+}
+
+app.get('/',(req,res)=> {
+res.send(obj)
+})
+
+app.get(endPoint,(req, res)=> {
+res.json(obj)
+})
+
+app.get(`${endPoint}/:id`, (req, res)=> {
+const {id} = req.params
+const result = obj.find(e => e.id === parseInt(id))
+if(!result){
+throw new Error('Holiiiiiii')
+}
+res.json(result)
+})
+
+app.post(endPoint, (req, res)=> {
+let data = req.body;
+obj.push(data)
+res.json(obj)
+} )
+
+app.use(errorHandler)
+app.use(errorLogger)
+
+app.listen(PORT, ()=> {
+console.log('escuchando en http://localhost')
+})
